aed525767775426e6bff41027b8b52c474b191db
LQuesadaM <luisquesada163@gmail.com>
2024-07-07 10:51:30 -0300

    🎉 begin project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { ContactForm } from './components/ContactForm'
+
+export const App = () => {
+  return (
+    <main className='bg-green-100 h-screen flex justify-center font-karla'>
+      <ContactForm />
+    </main>
+  )
+}
