d672a3d94afbaae3371830d098cb2d498eb3491c
LQuesadaM <luisquesada163@gmail.com>
2024-07-07 10:52:14 -0300

    🧱 set index import

---
+++ src/components/ui/index.js
@@ -0,0 +1,8 @@
+import { Label } from './Label.jsx'
+import { Input } from './Input.jsx'
+import { Button } from './Button.jsx'
+import { RadioGrup } from './RadioGrup.jsx'
+import { Textarea } from './Textarea.jsx'
+import { Paragraph } from './Paragraph.jsx'
+
+export { Label, Input, Button, RadioGrup, Textarea, Paragraph }
